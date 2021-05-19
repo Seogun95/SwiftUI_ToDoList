8cbc4686892d9fd43ad4b82a4501901b2113830f
SeonJungKim <ksj083895@gmail.com>
2021-05-19 20:37:34 +0500

    Initial Commit

---
+++ Shared/SwiftUI_ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ToDoListApp.swift
+//  Shared
+//
+//  Created by Seogun Kim on 2021/05/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
